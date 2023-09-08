c0f599fefa473e933788cfb9c3d8cb1990040b6f
mrsmylz <ernestcmadu@gmail.com>
2023-09-07 23:42:40 -0700

    puts.c

---
+++ 0x00-hello_world/4.puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+/** 
+  * main - entry point 
+  * 
+  * Return:0 Always(Success) 
+  */ 
+ int main(void) 
+ { 
+         puts("\"Programming is like building a multilingual puzzle"); 
+         return (0); 
+ }
