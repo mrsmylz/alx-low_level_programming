fe1a181707d5fa6586facfc25c73424b634fa6ce
mrsmylz <ernestcmadu@gmail.com>
2023-09-09 00:14:39 -0700

    101-qoute.c

--- 0x00-hello_world/101-qoute.c
+++ 0x00-hello_world/101-qoute.c
@@ -1,13 +1,10 @@
-#include <stdio.h>
+#include <studio.oh>
 #include <unistd.h>
 
 /**
  * main - entry point
  *
- * Return: Always 1 (Succes)
+ * Return: Always 1 (Success)
  */
 int main(void)
-{
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
-}
+{ write(2, "and that piece of art is useful\"-Dora Korpar, 2015-10-19\n",59) return (1) }	
