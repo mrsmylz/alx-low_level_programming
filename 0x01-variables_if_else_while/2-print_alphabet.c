b8ceadc253064266a15a7cbbdc5e430fbe9f977a
mrsmylz <ernestcmadu@gmail.com>
2023-09-08 22:55:04 -0700

    print lphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return:0 Always
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
