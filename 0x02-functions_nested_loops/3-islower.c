59534ce701eae266cabdff26986b836e4570ba43
mrsmylz <ernestcmadu@gmail.com>
2023-09-15 16:21:10 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - Entry point
+ *@c: the character to be checked
+ * Return:1 else 0
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
