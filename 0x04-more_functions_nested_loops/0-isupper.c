0e4d67c1dd13eec7aa78135a50bed08f9cfcdfea
mrsmylz <ernestcmadu@gmail.com>
2023-09-15 17:49:52 -0700

    super c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,20 +1,15 @@
-#include <stdio.h>
-/**
- * main - Entry point
- *
- * Return:0 Always
- */
-int main(void)
-{
-	 putchar('_');
-	 putchar('p');
-	 putchar('u');
-	 putchar('t');
-	 putchar('c');
-	 putchar('h');
-	 putchar('a');
-	 putchar('r');
-	 putchar('\n');
+#include "main.h"
 
-	return (0);
+/**
+ * _isupper - checks if for uppercase character
+ * @c: the character to be checked
+ * Return: returns 1 is c is uppercase, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
 }
