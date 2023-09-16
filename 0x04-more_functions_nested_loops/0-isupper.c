0d961a15abffce0f73a4cb57112bc246b0c759b5
mrsmylz <ernestcmadu@gmail.com>
2023-09-15 17:18:28 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - A function
+ * @c: parameter to be evaluated
+ * Return: 1 if true or 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
